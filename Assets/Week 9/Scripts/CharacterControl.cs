a0cb920a5c767a84f77dc0399496dfa7ab0f17e0
Adam Anderson <anderada@sheridancollege.ca>
2024-03-15 10:18:16 +0000

    task 4b: text element displays selected villager type

--- Assets/Week 9/Scripts/CharacterControl.cs
+++ Assets/Week 9/Scripts/CharacterControl.cs
@@ -2,9 +2,26 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+
 
 public class CharacterControl : MonoBehaviour
 {
+    public TextMeshProUGUI villagerDisplay;
+
+    void Start(){
+        villagerDisplay.text = "None Selected";
+    }
+
+    void FixedUpdate(){
+        if(SelectedVillager != null){
+            villagerDisplay.text = SelectedVillager.GetType().ToString();
+        }
+        else {
+            villagerDisplay.text = "None Selected";
+        }
+    }
+
     public static Villager SelectedVillager { get; private set; }
     public static void SetSelectedVillager(Villager villager)
     {
