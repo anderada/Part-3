8a899418b33088fc228faeb6da0332c4ce84fd5e
Adam Anderson <anderada@sheridancollge.ca>
2024-03-28 14:26:07 +0000

    Week11 task 1: square and triangle coroutines

---
+++ Assets/Week 11/Scripts/Growing.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Growing : MonoBehaviour
+{
+    public GameObject square;
+    public GameObject triangle;
+    public GameObject circle;
+    public TextMeshProUGUI squareTMP;
+    public TextMeshProUGUI triangleTMP;
+    public TextMeshProUGUI circleTMP;
+    public TextMeshProUGUI crTMP;
+    public int running;
+    Coroutine coroutine;
+
+    void Start()
+    {
+        StartCoroutine(Triangle());
+        Circle();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Square()
+    {
+        running++;
+
+        float size = 0;
+        while (size < 5)
+        {
+            size += Time.deltaTime;
+            Vector3 scale = new Vector3(size, size, size);
+            square.transform.localScale = scale;
+            squareTMP.text = "Square: " + scale;
+            yield return null;
+        }
+
+        running--;
+    }
+    IEnumerator Triangle()
+    {
+        running++;
+
+        yield return StartCoroutine(Square());
+
+        float size = 0;
+        while (size < 5)
+        {
+            size += Time.deltaTime;
+            Vector3 scale = new Vector3(size, size, size);
+            triangle.transform.localScale = scale;
+            triangleTMP.text = "Triangle: " + scale;
+            yield return null;
+        }
+
+        running--;
+    }
+    void Circle()
+    {
+
+        float size = 0;
+        while (size < 5)
+        {
+            size += Time.deltaTime;
+            Vector3 scale = new Vector3(size, size, size);
+            circle.transform.localScale = scale;
+            circleTMP.text = "Cirlce: " + scale;
+        }
+        while (size > 0)
+        {
+            size -= Time.deltaTime;
+            Vector3 scale = new Vector3(size, size, size);
+            circle.transform.localScale = scale;
+            circleTMP.text = "Cirlce: " + scale;
+        }
+    }
+}
